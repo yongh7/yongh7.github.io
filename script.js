fb7bd13487379dca1e4da9f231914d08ac0106a5
acneyouth1996 <yh849@cornell.edu>
2023-12-15 13:43:57 -0800

    new temp

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you!";
+  question.innerHTML = "Yay, thank you!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
